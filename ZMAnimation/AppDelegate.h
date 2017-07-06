d1682f6e90fca988d741f2ae79591dce8bb838db
shengguangdaren <w532080396@163.com>
2017-07-06 21:02:06 +0800

    Initial Commit

---
+++ ZMAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZMAnimation
+//
+//  Created by 圣光大人 on 2017/7/6.
+//  Copyright © 2017年 圣光大人. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
